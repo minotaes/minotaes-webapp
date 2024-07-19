40ed93e51f6794cbb8592e2947297e20885a6b9a
eddy <mauricio.gonsales.23@outlook.com>
2024-07-18 19:49:52 -0600

    minota h2

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,7 +4,8 @@ import React from 'react'
 const Page = () => {
   return (
     <div>
-      <h1>Paginaa </h1>
+      <h1>Paginaaaaaaaaa  </h1>
+      <h2>minota</h2>
       <Link href={'/'}>Volver al inicio</Link>
     </div>
   )
