93d46ed516365923ec4a172960f6bcd8b150c89e
eddy <mauricio.gonsales.23@outlook.com>
2024-07-18 19:47:22 -0600

    go back update

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Page = () => {
   return (
     <div>
-      <h1>home </h1>
+      <h1>Paginaa </h1>
+      <Link href={'/'}>Volver al inicio</Link>
     </div>
   )
 }
