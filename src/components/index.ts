e04e46651806836bcf782b281cb6245aadf2c8a0
diegoleondev <diegoleon.dev@gmail.com>
2024-07-20 16:57:30 -0600

    export all components

---
+++ src/components/index.ts
@@ -0,0 +1,15 @@
+export * from "./button/async";
+export * from "./button/embed";
+export * from "./button/link";
+export * from "./button/normal";
+
+export * from "./loader/spinner";
+
+export * from "./texts/anchor";
+export * from "./texts/main-title";
+export * from "./texts/paragraph";
+export * from "./texts/strong";
+export * from "./texts/subtitle";
+export * from "./texts/text-skeleton";
+export * from "./texts/text";
+export * from "./texts/title";
