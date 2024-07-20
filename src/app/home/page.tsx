675b10621eb2350ddb5698044914e553ccabdd89
diegoleondev <diegoleon.dev@gmail.com>
2024-07-20 16:45:32 -0600

    [patch] formatted files

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Page = () => {
   return (
     <div>
       <h1>home </h1>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
