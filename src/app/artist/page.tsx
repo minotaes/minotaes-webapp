9c610cd9f9b642c4e8d952cb84c260c9306d3810
eddy <mauricio.gonsales.23@outlook.com>
2024-07-24 00:02:15 -0600

    Correcion de estilos e implementacion de subtitulos predeterminados

--- src/app/artist/page.tsx
+++ src/app/artist/page.tsx
@@ -1,11 +1,25 @@
 import React from "react";
-
 import Header from "./components/header";
+
+import styles from "./artist.module.css";
+import { Subtitle } from "@/components";
+
+const defaultSubtitleStyle = {
+  size: "small" as const,
+  color: "primary" as const, // Puede cambiar
+};
 
 const Artist = () => {
   return (
-    <div>
+    <div className={styles.page}>
       <Header />
+      <div className={styles.container}>
+        <Subtitle {...defaultSubtitleStyle}>Eventos</Subtitle>
+        <Subtitle {...defaultSubtitleStyle}>Música</Subtitle>
+        <Subtitle {...defaultSubtitleStyle}>
+          Recomendaciones del artista
+        </Subtitle>
+      </div>
     </div>
   );
 };
