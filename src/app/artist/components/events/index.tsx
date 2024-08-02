c548c8d58598b90a43ea72e6d238d32eeb5db732
eddy <mauricio.gonsales.23@outlook.com>
2024-08-01 21:19:29 -0600

    componente events

--- src/app/artist/components/events/index.tsx
+++ src/app/artist/components/events/index.tsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { Text } from "@/components";
+import styles from "./events.module.css";
 
 const Events = () => {
   return (
-    <div>
-      <h1>Events</h1>
-    </div>
+    <section className={styles.container}>
+      <aside className={styles.square}>
+        <Text size="xsmall" weight="bold">
+          Sep
+        </Text>
+        <Text size="xsmall">10</Text>
+      </aside>
+      <div className={styles.details}>
+        <Text size="small" weight="bold">
+          Morelia, Michoacán
+        </Text>
+        <Text size="small">Lun 7:00 - Cactux</Text>
+      </div>
+    </section>
   );
 };
 
